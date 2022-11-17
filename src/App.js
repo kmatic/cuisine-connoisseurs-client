720ead32b488e79e4a74cde493ac3e08211305c1
Kristopher <kristopher_matic@hotmail.com>
2022-11-16 17:29:03 -0800

    Simple landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Landing from './pages/Landing'
 
 const App = () => {
     return (
         <>
             <Header />
+            <main className="bg-slate-100">
+                <Landing />
+            </main>
             {/* <Footer /> */}
         </>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ const Footer = () => {
     return (
         <div>
             <footer className="bg-slate-700 text-white flex justify-center items-center p-2">
-                <a href="" className="flex gap-2">
+                <a
+                    href="https://github.com/kmatic/cuisine-connoisseurs-client"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-2">
                     kmatic
                     <FaGithub className="text-2xl" />
                 </a>
