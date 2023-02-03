60a5f44832c87b7007ce405d9c8c082ee9bfc743
Kristopher <kristopher_matic@hotmail.com>
2023-02-03 12:28:04 -0800

    Switch from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import {
     Header,
     Landing,
